d51eaec1704103f766bd6ba47e78e48c59cf2d98
Emili Sommer <eliza.liebt.sommer@gmail.com>
2025-10-15 16:22:16 +0200

    Builder Computer

--- Creational - Exercises/ComputerAssembly_exercise/ComputerDirector.java
+++ Creational - Exercises/ComputerAssembly_exercise/ComputerDirector.java
@@ -1,3 +1,12 @@
 public class ComputerDirector {
-    
+    public void constructComputer(IComputerBuilder builder) {
+        builder.buildMotherboard();
+        builder.buildCpu();
+        builder.buildRam();
+        builder.buildStorage();
+        builder.buildGraphicsCard();
+        builder.buildPowerSupply();
+        builder.buildComputerCase();
+        builder.buildCoolingSystem();
+    }
 }
